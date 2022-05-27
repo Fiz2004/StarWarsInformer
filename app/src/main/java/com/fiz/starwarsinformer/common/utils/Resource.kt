9affa5e3e7dd92e2c10cc1bb43c81fe33b0fcd8d
Fiz2004 <Fiz2004@Rambler.ru>
2022-05-27 10:01:38 +0700

    add util class Resource

---
+++ app/src/main/java/com/fiz/starwarsinformer/common/utils/Resource.kt
@@ -0,0 +1,7 @@
+package com.fiz.starwarsinformer.common.utils
+
+sealed class Resource<T>(val data: T? = null, val message: Int? = null) {
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Error<T>(message: Int, data: T? = null) : Resource<T>(data, message)
+    class Loading<T>(val isLoading: Boolean = true) : Resource<T>(null)
+}
